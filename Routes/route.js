b802d887658e47d7754aba23e788c90841920b16
Usman Rajput <bcawala3@gmail.com>
2023-11-02 23:20:19 +0530

    router level middleware

--- Routes/route.js
+++ Routes/route.js
@@ -1,5 +1,8 @@
 const router = require("express").Router();
 
+// <----- use middleware ----->
+const middleware = require("../MiddleWare/ApiKey");
+
 router.get("/", (req, res) => {
   res.render("index", {
     title: "Welcome to home page!!",
@@ -10,6 +13,20 @@ router.get("/about", (req, res) => {
   res.render("about", {
     title: "Welcome to about page!!",
   });
+});
+
+// <----- use middleware ----->
+router.get("/product/api", middleware, (req, res) => {
+  res.json([
+    {
+      id: "123",
+      name: "hlo",
+    },
+    {
+      id: "1234",
+      name: "how are you",
+    },
+  ]);
 });
 
 module.exports = router;
